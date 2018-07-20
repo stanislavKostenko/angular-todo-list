b396d0095165543b7bacfd33f98caddd717eb9a2
Mike.Gusev <mike.gusev.payever@gmail.com>
2018-07-20 13:10:23 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoFormComponent } from './todo/todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule
